15d4aec48104b52aebe466035a5c02038552637e
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-03-21 22:04:54 -0600

    setup: express and mongodb

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-const express = require('express');
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
 
-const app = express();
-const port = 3000;
+require('dotenv/config')
+
+const app = express()
+const port = process.env.PORT
+const api = process.env.API_URL
+
+app.use(express.json())
+app.use(morgan('tiny'))
+
+app.use(api, require('./routes/products'))
+
+mongoose.connect(process.env.DB_CONNECT)
+  .then(() => {
+    console.log('Db Connection is ready');
+  })
+  .catch((e) => {
+    console.log(e);
+  })
 
 app.listen(port, () => {
-  console.log('server running');
+  console.log(`https://localhost:${port}`)
 })
