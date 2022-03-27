1a2bbca9d3264eeccdcc1200f7c25583965914db
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-03-27 00:40:36 -0600

    created: dbconnection

---
+++ database/dbConnection.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.DB_CNN)
+        console.log('DB online');
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error a la hora de inicializar DB');
+    }
+}
+
+module.exports = {
+    dbConnection
+};
