c20c0b84a887e95108397ad7f50e150ec5c9c520
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-03-29 13:13:59 -0600

    Updated: product schema

--- models/product.schema.js
+++ models/product.schema.js
@@ -17,10 +17,44 @@ const productSchema = mongoose.Schema({
     type: String,
     required: true
   },
+  images: [{
+    type: String
+  }],
   countInStock: {
     type: Number,
     required: true,
+    min: 0,
+    max: 500
   },
+  brand: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    default: 0
+  },
+  rating: {
+    type: Number,
+    default: 0
+  },
+  numReviews: {
+    type: Number,
+    default: 0
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  }
 })
 
 exports.product = mongoose.model('Product', productSchema);
