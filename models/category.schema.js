312010c73a3a6b68031aebf930c29beb95c2a6e9
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-03-29 14:41:24 -0600

    updated: category schema

--- models/category.schema.js
+++ models/category.schema.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 
 const categorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+    required: true
+  }
 
 })
 
