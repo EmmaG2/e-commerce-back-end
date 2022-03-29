af3f36a58bff513323d5136cbf18c0a0ecb92152
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-03-28 23:29:10 -0600

    created: orders route

---
+++ routes/orders.route.js
@@ -0,0 +1,13 @@
+const { Order } = require('../models/order.schema');
+const { Router } = require('express')
+
+const router = Router()
+
+router.get(`/`, async (req, res) =>{
+  const orderList = await Order.find();
+
+  if(!orderList) res.status(500).json({success: false})
+  res.send(orderList);
+})
+
+module.exports = router;
