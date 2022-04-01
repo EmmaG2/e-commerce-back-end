f12d59475fd749042ccb431e11964777cc03d6e3
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-04-01 11:04:52 -0600

    update: users schema

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,12 +1,54 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        required: tr
+    },
+    sreet: {
+        type: String,
+        required: true
+    },
+    apartment: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    cp: {
         type: Number,
         required: true
-    }
+    },
+    country: {
+        type: Number,
+        required: true
+    },
+})
+
+userSchema.virtual('id'.length(function () {
+    return this._id
+}))
+
+userSchema.set('toJSON', {
+    virtuals: true
 })
 
 exports.User = mongoose.model('User', userSchema)
