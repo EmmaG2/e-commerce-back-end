49402aefaa35c4508dd850bf52856792b7024f88
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-04-22 15:32:46 -0500

    updated: users route

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,10 +1,9 @@
-const { User } = require('../models/user.schema')
-const { Router } = require('express')
+const {User} = require('../models/user.schema')
+const {Router} = require('express')
 
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const router = Router()
-
 
 // TODO: POST Methods
 router.post(`/users`, (req, res) => {
@@ -48,7 +47,6 @@ router.post(`/users/login`, async (req, res) => {
   } else {
     res.status(400).send('password is wrong')
   }
-
 })
 
 router.post(`/users/register`, async (req, res) => {
@@ -72,60 +70,60 @@ router.post(`/users/register`, async (req, res) => {
 
   user.save()
   res.status(200).json({
-    message: "succesful regist",
-    user 
+    message: 'succesful regist',
+    user,
   })
 })
 
 // TODO: GET Methods
 router.get(`/users`, (req, res) => {
-  User.find().select('userName realName phone email')
-    .then(user => {
-      if (!user) return res.status(404).json({
-        succes: false,
-        message: 'no se encontró el usuario'
-      })
+  User.find()
+    .select('userName realName phone email')
+    .then((user) => {
+      if (!user)
+        return res.status(404).json({
+          succes: false,
+          message: 'no se encontró el usuario',
+        })
 
       return res.status(200).send(user)
     })
 })
 
 router.get(`/users/:id`, async (req, res) => {
-
   const user = await User.findById(req.params.id)
 
-  if (!user) res.status(404).json({
+  if (!user)
+    res.status(404).json({
       succes: false,
-      message: 'El usuario no fue encontrado'
-  })
+      message: 'El usuario no fue encontrado',
+    })
 
   res.status(200).send(user)
 })
 
 router.get(`/users/get/count`, (req, res) => {
-
-  User.countDocuments()
-    .then(usersCount => {
-      if (!usersCount) return res.status(500).json({
+  User.countDocuments().then((usersCount) => {
+    if (!usersCount)
+      return res.status(500).json({
         succes: false,
-        message: 'No se encontraron usuarios'
+        message: 'No se encontraron usuarios',
       })
 
-      return res.status(200).send({usersCount})
-    })
+    return res.status(200).send({usersCount})
+  })
 })
 
 // TODO: PUT METHODS
 router.put(`/users/:id`, async (req, res) => {
+  const userExist = await User.findById(req.params.id)
 
-  const userExist = await User.findById(req.params.id);
+  let newPassword
 
-  let newPassword;
-
-  if (req.body.password) {newPassword = bcrypt.hashSync(req.body.password, 10)}
-  else newPassword = userExist.password
+  if (req.body.password) {
+    newPassword = bcrypt.hashSync(req.body.password, 10)
+  } else newPassword = userExist.password
   const user = await User.findByIdAndUpdate(
-    
     req.params.id,
     {
       userName: req.body.userName,
@@ -145,35 +143,34 @@ router.put(`/users/:id`, async (req, res) => {
       country: req.body.country,
     },
     {
-      new: true
+      new: true,
     }
   )
 
-    if (!user) return res.status(404).json({
+  if (!user)
+    return res.status(404).json({
       succes: false,
-      message: 'No se pudo actualizar la categoria'
+      message: 'No se pudo actualizar la categoria',
     })
 
-    res.send(user)
+  res.send(user)
 })
-
 
 // TODO: DELETE routes
 
 router.delete(`/users/:id`, (req, res) => {
-
-  User.findByIdAndDelete(req.params.id)
-    .then(user => {
-      if (!user) return res.status(404).json({
+  User.findByIdAndDelete(req.params.id).then((user) => {
+    if (!user)
+      return res.status(404).json({
         succes: false,
-        message: 'user can\'t be deleted'
+        message: "user can't be deleted",
       })
 
-      return res.status(200).json({
-        succes: true,
-        message: 'User has been deleted'
-      })
+    return res.status(200).json({
+      succes: true,
+      message: 'User has been deleted',
     })
+  })
 })
 
 module.exports = router
