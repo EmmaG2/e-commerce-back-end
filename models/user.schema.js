3d9ee71b39b070c6db7962df9d8ea43c07d03548
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-04-22 15:31:04 -0500

    updated: user schema

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,66 +1,66 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    userName: {
-        type: String,
-        required: true
-    },
-    realName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: String,
-        required: true,
-    },
-    isAdmin: {
-        type: Boolean,
-        required: true
-    },
-    estado: {
-        type: String,
-        required: true
-    },
-    municipio: {
-        type: String,
-        required: true
-    },
-    colonia: {
-        type: String,
-        required: true
-    },
-    numExterior: {
-        type: String,
-        required: true
-    },
-    numeInterior: {
-        type: String,
-        required: false
-    },
-    calle: {
-        type: String,
-        required: true
-    },
-    ciudad: {
-        type: String,
-        required: true
-    },
-    cp: {
-        type: Number,
-        required: true
-    },
-    country: {
-        type: String,
-        required: true
-    },
+  userName: {
+    type: String,
+    required: true,
+  },
+  realName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+  },
+  estado: {
+    type: String,
+    required: true,
+  },
+  municipio: {
+    type: String,
+    required: true,
+  },
+  colonia: {
+    type: String,
+    required: true,
+  },
+  numExterior: {
+    type: String,
+    required: true,
+  },
+  numeInterior: {
+    type: String,
+    required: false,
+  },
+  calle: {
+    type: String,
+    required: true,
+  },
+  ciudad: {
+    type: String,
+    required: true,
+  },
+  cp: {
+    type: Number,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
 })
 
 exports.User = mongoose.model('User', userSchema)
